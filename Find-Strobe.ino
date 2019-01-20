4538a0016479e77e8e90b816025ca1fbedf5e97f
Victor <ua6em@yandex.ru>
2019-01-20 20:56:54 +0300

    Update Find-Strobe.ino
    Исправлен тип функци, процедура использования

--- Find-Strobe.ino
+++ Find-Strobe.ino
@@ -21,12 +21,12 @@ void setup() {
   uint8_t prescaler = PRESCALER_1024;
   uint16_t topValue = 1562;
   pinMode(9, OUTPUT);
-  TCCR1A = 0x40;              //  Инвертирование пина 9 по сравнению
-  TCCR1B = 0x08 | prescaler;  // Установить СТС режим и делитель частоты
-  OCR1A = topValue;           //  установить TOP равным topValue
+  TCCR1A = 0x40;               //  Инвертирование пина 9 по сравнению
+  TCCR1B = 0x08 | prescaler;   // Установить СТС режим и делитель частоты
+  OCR1A = topValue;            //  установить TOP равным topValue
 }
 
- int strobe_high(int pin){
+boolean strobe_high(int pin){
  int result;
  if(digitalRead(pin)== HIGH && flag == 0){
  flag=1; result = 1;} else{ result = 0;}
@@ -34,7 +34,7 @@ void setup() {
  return result;
 }
 
-int strobe_low(int pin){
+boolean strobe_low(int pin){
  int result;
  if(digitalRead(pin)== LOW && flag == 0){
  flag=1; result = 1;} else{ result = 0;}
@@ -44,8 +44,7 @@ int strobe_low(int pin){
 
 void loop() {
   digitalWrite(13,digitalRead(9));
- // if (int strobe=(strobe_high(pin_strobe)) == 1) digitalWrite(1,!digitalRead(1));
-//  if (int strobe=(strobe_low(pin_strobe)) == 1) digitalWrite(1,!digitalRead(1));
-  if (strobe_low(pin_strobe)) digitalWrite(1,!digitalRead(1));
+ if (strobe_high(pin_strobe)) digitalWrite(1,!digitalRead(1));
+//if (strobe_low(pin_strobe)) digitalWrite(1,!digitalRead(1));
   
 }
